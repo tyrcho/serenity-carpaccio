e08a8bbc9853cfb6c4c8313c4279582fe7744c89
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-02-02 17:25:29 +0100

    changed dependencies to use serenity with cucumber

--- src/test/java/demo/RunCukesTest.java
+++ src/test/java/demo/RunCukesTest.java
@@ -2,10 +2,11 @@ package demo;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources")
 public class RunCukesTest {
 
 
